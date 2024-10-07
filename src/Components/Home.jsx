e6b8afc5c3f65a66f1826c2814e0b7ae548860c1
Sanjay Patel <sanjaypatel5346@gmail.com>
2024-10-07 08:29:50 +0530

    added Loading component and also displayed all products images and it's titles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,33 +2,40 @@ import React, { useContext } from "react";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
 import { ProductContext } from "../utils/context";
+import Loading from "./Loading";
 
 function Home() {
+  const [products] = useContext(ProductContext);
 
-  const [products] = useContext(ProductContext)
+  // if (!products) return <div>Loading...</div>;
 
-  if (!products) return <div>Loading...</div>; 
-  console.log(products)
+  console.log(products);
 
-  return (
+  return products ? (
     <>
       <Nav />
       <div className="h-full w-[85%] pt-[5%] p-10 flex flex-wrap overflow-x-hidden overflow-y-auto">
-        <Link
-          to="/details/1"
-          className="card w-[18%] h-[30vh] mr-5 p-3 border shadow rounded flex flex-col items-center justify-center"
-        >
-          <div
-            className="w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3 transform hover:scale-110 transition duration-900 ease-in-out cursor-pointer"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1 className="transform hover:scale-105 cursor-pointer">Heading</h1>
-        </Link>
+        {products.map((p, i) => {
+          return (<Link
+            to="/details/1"
+            className="card w-[18%] h-[30vh] mr-5 p-3 border shadow rounded flex flex-col items-center justify-center"
+          >
+            <div
+              className="w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3 transform hover:scale-110 transition duration-900 ease-in-out cursor-pointer"
+              style={{
+                backgroundImage:
+                  `url(${p.image})`,
+              }}
+            ></div>
+            <h1 className="transform hover:scale-105 cursor-pointer">
+              {p.title}
+            </h1>
+          </Link>);
+        })}
       </div>
     </>
+  ) : (
+    <Loading />
   );
 }
 
@@ -37,3 +44,5 @@ export default Home;
 // overflow-x-hidden-> (hidden) -> Prevents horizontal (x-axis) scrolling, ensuring the card stays fixed and doesn't move or slide sideways
 
 // overflow-y-auto-> (auto) -> Allows vertical (y-axis) scrolling if content exceeds the container's height
+
+// 59:00 Loading
